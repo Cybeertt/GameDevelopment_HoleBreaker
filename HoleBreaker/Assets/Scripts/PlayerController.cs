51b0d3bc2f856b75d6aa94f620dcc9d85950e41f
Daniel Lachkeev <74510099+daniel-lachkeev@users.noreply.github.com>
2021-06-23 16:47:28 +0100

    shift to move faster

--- HoleBreaker/Assets/Scripts/PlayerController.cs
+++ HoleBreaker/Assets/Scripts/PlayerController.cs
@@ -6,15 +6,22 @@ public class PlayerController : MonoBehaviour
 {
     public CharacterController controller;
     public float speed = 12f;
+    public float speedMultiplier = 2.0f;
+    private float currentSpeed;
 
     void Update()
     {
+        currentSpeed = speed;
         float x = Input.GetAxis("HorizontalX");
         float z = Input.GetAxis("HorizontalZ");
         float y = Input.GetAxis("Vertical");
 
         Vector3 move = transform.right * x + transform.up * y + transform.forward * z;
 
-        controller.Move(move * speed * Time.deltaTime);
+        if (Input.GetKey(KeyCode.LeftShift)) {
+            currentSpeed = currentSpeed * speedMultiplier;
+        }
+
+        controller.Move(move * currentSpeed * Time.deltaTime);
     }
 }
