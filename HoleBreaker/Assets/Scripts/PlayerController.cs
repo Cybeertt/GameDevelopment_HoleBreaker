5dc169b63cbb82edd7a813cd51c4b2e70ac1fe7e
Daniel Lachkeev <74510099+daniel-lachkeev@users.noreply.github.com>
2021-06-23 17:39:01 +0100

    player fps component

---
+++ HoleBreaker/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public CharacterController controller;
+    public float speed = 12f;
+    public float speedMultiplier = 2.0f;
+    public float verticalMultiplier = 2.0f;
+    private float currentSpeed;
+
+    void Update()
+    {
+        currentSpeed = speed;
+        float x = Input.GetAxis("HorizontalX");
+        float z = Input.GetAxis("HorizontalZ");
+        float y = Input.GetAxis("Vertical");
+
+        Vector3 move = transform.right * x + transform.up * y * verticalMultiplier + transform.forward * z;
+
+        if (Input.GetKey(KeyCode.LeftShift)) {
+            currentSpeed = currentSpeed * speedMultiplier;
+        }
+
+        controller.Move(move * currentSpeed * Time.deltaTime);
+    }
+}
